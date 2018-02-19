f6586350c0b4fc4ed19319035e9128bf03e21411
anishraja100 <anishraja200@yahoo.com>
2018-02-19 17:59:54 -0500

    Initial commit

---
+++ run.sh
@@ -0,0 +1,6 @@
+python3 label_image/label_image.py \
+--graph=output_graph.pb --labels=output_labels.txt \
+--input_layer=Mul \
+--output_layer=final_result \
+--input_mean=128 --input_std=128 \
+--image=images.jpg
