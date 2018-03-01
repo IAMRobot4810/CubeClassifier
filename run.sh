9d67808e0e2db92ec623d20f1efa191d44fab8ce
Joe Maples <joe@frap129.org>
2018-03-01 13:37:20 -0500

    Modify run.sh for MobileNet
    Signed-off-by: Joe Maples <joe@frap129.org>

--- run.sh
+++ run.sh
@@ -1,6 +1,7 @@
 python3 label_image/label_image.py \
 --graph=quantized_graph.pb --labels=output_labels.txt \
---input_layer=Mul \
+--input_layer=input \
 --output_layer=final_result \
+--input_height=224 --input_width=224 \
 --input_mean=128 --input_std=128 \
 --image=images.jpg
